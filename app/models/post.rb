b5bad0bd294e3213cf2e6090d3b96105758a0141
Munezero Christian <chamsf5@aol.com>
2022-07-07 12:01:59 +0200

    Add methods for processing data

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
+  has_many :likes, foreign_key: :post_id
+  has_many :comments, foreign_key: :post_id
+
+  def update_posts_counter
+    author.update(posts_counter: author.posts.count)
+  end
+
+  def last_five_comments
+    comments.last(5)
+  end
 end

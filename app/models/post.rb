9e2ea0b62a97f75ac7393506a0c27e7eaddd827a
Munezero Christian <chamsf5@aol.com>
2022-07-16 00:51:46 +0200

    Modified post, comment and like models

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,6 +7,8 @@ class Post < ApplicationRecord
   validates :comments_counter, comparison: { greater_than_or_equal_to: 0 }
   validates :likes_counter, comparison: { greater_than_or_equal_to: 0 }
 
+  after_save :update_posts_counter
+
   def update_posts_counter
     author.update(posts_counter: author.posts.count)
   end

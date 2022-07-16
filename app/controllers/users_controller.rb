71e6a2e0937854360109b2967ff18133ce2af075
Munezero Christian <chamsf5@aol.com>
2022-07-16 04:07:16 +0200

    Allow users to add posts, comments and likes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,11 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all
+    @current_user = ApplicationController.new.current_user
+    @users = User.all.order(updated_at: :desc)
   end
 
   def show
+    @current_user = ApplicationController.new.current_user
     @user = User.find(params[:id])
     @posts = @user.last_three_posts
   end

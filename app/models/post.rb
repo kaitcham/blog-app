26e954dc3882baf9314952603d82262136a949a1
Munezero Christian <chamsf5@aol.com>
2022-07-22 12:09:36 +0200

    Modified post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,13 +1,14 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
-  has_many :likes, foreign_key: :post_id
-  has_many :comments, foreign_key: :post_id
+  has_many :likes, dependent: :destroy, foreign_key: :post_id
+  has_many :comments, dependent: :destroy, foreign_key: :post_id
 
   validates :title, presence: true, length: { maximum: 250 }
   validates :comments_counter, comparison: { greater_than_or_equal_to: 0 }
   validates :likes_counter, comparison: { greater_than_or_equal_to: 0 }
 
   after_save :update_posts_counter
+  after_destroy :update_posts_counter
 
   def update_posts_counter
     author.update(posts_counter: author.posts.count)

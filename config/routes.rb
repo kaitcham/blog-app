3ff641bb444086c0ddd29a6afb444664e621af60
Munezero Christian <chamsf5@aol.com>
2022-07-23 14:39:18 +0200

    Revert "Api endpoint"

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  scope :api, defaults: { format: :json } do
-    devise_for :users, controllers: { sessions: :sessions, registrations: :registrations}
-  end
-
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
@@ -14,9 +11,4 @@ Rails.application.routes.draw do
       resources :likes, only: [:new, :create]
     end
   end
-
-  get '/api/users/:user_id/posts', to: 'api#user_posts', as: 'api_user_posts'
-  get '/api/users/:user_id/posts/:post_id/comments', to: 'api#user_post_comments', as: 'api_posts_comments'
-  post '/api/users/:user_id/posts/:post_id/comments', to: 'api#new_comment', as: 'new_post_comment'
-
 end
